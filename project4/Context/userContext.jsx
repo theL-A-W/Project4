a7dd29d0fb3d7f6546bac17c25712477864bf79d
Lindsay Walker <lawalker98@gmail.com>
2023-12-05 20:03:56 -0500

    update

--- project4/Context/userContext.jsx
+++ project4/Context/userContext.jsx
@@ -1,36 +1,58 @@
-import React, { createContext, useContext, useState } from 'react';
+// userContext.js
+import React, { createContext, useContext, useReducer, useEffect } from 'react';
 
+// Define initial state shape
+const initialState = {
+  user: localStorage.getItem('user') || '',
+  token: localStorage.getItem('token') || '',
+};
+
+// Define actions for updating the state
+const userReducer = (state, action) => {
+  switch (action.type) {
+    case 'SET_USER':
+      return { ...state, user: action.payload };
+    case 'SET_TOKEN':
+      return { ...state, token: action.payload };
+    case 'LOGOUT':
+      return { user: '', token: '' };
+    default:
+      return state;
+  }
+};
+
+// Create context
 const UserContext = createContext();
 
-export const UserProvider = ({ children }) => {
-  const [userState, setUserState] = useState({
-    user: '',
-    token: '',
-  });
+// Create context provider
+const UserProvider = ({ children }) => {
+  const [userState, dispatch] = useReducer(userReducer, initialState);
 
-  const setUser = (username, token) => {
-    setUserState({ username, token });
-  };
+  // Save state to localStorage on changes
+  useEffect(() => {
+    localStorage.setItem('user', userState.user);
+    localStorage.setItem('token', userState.token);
+  }, [userState]);
 
-
-  const logout = () => {
-    setUserState({
-      user: '',
-      token: '',
-    });
-  };
+  // Define actions to update the state
+  const setUser = (user) => dispatch({ type: 'SET_USER', payload: user });
+  const setToken = (token) => dispatch({ type: 'SET_TOKEN', payload: token });
+  const logout = () => dispatch({ type: 'LOGOUT' });
 
   return (
-    <UserContext.Provider value={{ userState, setUser, logout }}>
+    <UserContext.Provider value={{ userState, setUser, setToken, logout }}>
       {children}
     </UserContext.Provider>
   );
 };
 
-export const useUser = () => {
+// Custom hook to consume the context
+const useUser = () => {
   const context = useContext(UserContext);
   if (!context) {
     throw new Error('useUser must be used within a UserProvider');
   }
   return context;
 };
+
+export { UserProvider, useUser };
