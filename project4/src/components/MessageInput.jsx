a1b1f535f2bd657962774ace1ba358669076d367
Lindsay Walker <lawalker98@gmail.com>
2023-12-06 21:51:02 -0500

    update

--- project4/src/components/MessageInput.jsx
+++ project4/src/components/MessageInput.jsx
@@ -1,15 +1,38 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { Card } from 'react-bootstrap';
+import { Card, Dropdown, DropdownMenu, Button, DropdownToggle, DropdownItem, Accordion, CardHeader} from 'react-bootstrap';
 import { useUser } from '../../Context/userContext';
+import { useAccordionButton } from 'react-bootstrap/AccordionButton';
+
 
 export default function MessageInput({ onSendMessage, selectedFriend, friendshipId, messages, users, sender }) {
   const [message, setMessage] = useState([]);
   const [allUsers, setAllUsers] = useState('');
   const { userState: { user, token } } = useUser();
   const [userMessages, setUserMessages] = useState([]);
+  const [isEditVisible, setEditVisibility] = useState(false);
 
 
+
+  function CustomToggle({ children, eventKey }) {
+
+    const decoratedOnClick = useAccordionButton(eventKey, () =>{
+    if (isEditVisible == false){
+      setEditVisibility(true)
+    }else{
+      setEditVisibility(false)
+    }
+    })
+  
+    return (
+      <Button id='message-dropdown-btn'
+        type="button"
+        onClick={decoratedOnClick}
+      >
+        {children}
+      </Button>
+    );
+  }
   const fetchUserMessages = async () => {
     try {
       const response = await axios.get(`http://localhost:8000/messages/?friend=${selectedFriend.receiverUsername}`, {
@@ -81,22 +104,51 @@ export default function MessageInput({ onSendMessage, selectedFriend, friendship
       setMessage('');
       fetchUserMessages()
   };
+const handleMessageDetails = () => {
+  if (isEditVisible == false){
+  setEditVisibility(true)
+}else{
+  setEditVisibility(false)
+}
+}
 
   return (
+
     <div className='message-input'>
-      <h2 id="selected-user-name">{selectedFriend.receiverUsername}</h2>
+      <h2 id='selected-user-name'>{selectedFriend.receiverUsername}</h2>
       <div className='message-display-window'>
-        {messages && userMessages.map((message) => (
-          <div key={message.id}>
-            <div>
-              <li id="text-messages"><Card>{message.content}</Card></li>
+        {messages &&
+          userMessages.map((message) => (
+            <div key={message.id}>
+              <div>
+                <Accordion defaultActiveKey='0'>
+                  <Card id='message-card'>
+                    <li id='text-messages'>
+                      <CustomToggle eventKey='0' onClick={handleMessageDetails} style={{ backgroundColor: message.user1 === selectedFriend.receiverId ? 'blue' : 'black' }}>
+                        {message.content}
+                      </CustomToggle>
+                    </li>
+                    <Accordion.Collapse eventKey='0'>
+                      <Card.Body id='edit-dropdown' style={{ display: isEditVisible ? 'block' : 'none' }}>
+                        Edit
+                      </Card.Body>
+                    </Accordion.Collapse>
+                  </Card>
+                </Accordion>
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
       </div>
       <div className='text-send'>
-        <textarea id="message-input" value={message} onChange={handleInputChange} placeholder="Type your message..." />
-        <button onClick={handleSendMessage} id="message-send-btn">Send</button>
+        <textarea
+          id='message-input'
+          value={message}
+          onChange={handleInputChange}
+          placeholder='Type your message...'
+        />
+        <button onClick={handleSendMessage} id='message-send-btn'>
+          Send
+        </button>
       </div>
     </div>
   );
